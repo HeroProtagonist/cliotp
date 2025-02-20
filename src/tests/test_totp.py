ff4cb108735c2f22df2e5aeee442e2db03ce6911
Jordan Tepper <12506217+HeroProtagonist@users.noreply.github.com>
2025-02-20 01:34:07 -0500

    Update totp tests

--- src/tests/test_totp.py
+++ src/tests/test_totp.py
@@ -6,8 +6,60 @@ from src.totp import Totp
 # Expected values from Appendix B
 class TotpTestCase(unittest.TestCase):
     def setUp(self):
-        self.totp = Totp("12345678901234567890")
+        self.secret = "12345678901234567890"
+        self.times = [
+            59,
+            1111111109,
+            1111111111,
+            1234567890,
+            2000000000,
+            20000000000,
+        ]
 
-    def test_it(self):
-        code = self.totp.generate_code(1111111109)
-        print(code)
+    def test_sha1(self):
+        expected_codes = [
+            94287082,
+            7081804,
+            14050471,
+            89005924,
+            69279037,
+            65353130,
+        ]
+
+        totp = Totp(self.secret)
+
+        codes = [totp.generate_code(t) for t in self.times]
+
+        self.assertEqual(expected_codes, codes)
+
+    def test_sha256(self):
+        expected_codes = [
+            46119246,
+            68084774,
+            67062674,
+            91819424,
+            90698825,
+            77737706,
+        ]
+
+        totp = Totp(self.secret, algorithm="sha256")
+
+        codes = [totp.generate_code(t) for t in self.times]
+
+        self.assertEqual(expected_codes, codes)
+
+    def test_sha512(self):
+        expected_codes = [
+            90693936,
+            25091201,
+            99943326,
+            93441116,
+            38618901,
+            47863826,
+        ]
+
+        totp = Totp(self.secret, algorithm="sha512")
+
+        codes = [totp.generate_code(t) for t in self.times]
+
+        self.assertEqual(expected_codes, codes)
--- src/totp.py
+++ src/totp.py
@@ -1,14 +1,16 @@
 from datetime import timezone, datetime
 from src.hotp import Hotp
 
+
 class Totp:
-    def __init__(self, secret, algorithm="sha1", time_step=30):
-        self.hotp = Hotp(secret)
+    def __init__(self, secret, algorithm="sha1", time_step=30, code_digits=8):
+        self.hotp = Hotp(secret, code_digits=code_digits, algorithm=algorithm)
         self.time_step = time_step
-        self.algorithm = algorithm
 
     def generate_code(self, current_timestamp):
-        current_timestamp = current_timestamp or int(datetime.now(tz=timezone.utc).timestamp())
+        current_timestamp = current_timestamp or int(
+            datetime.now(tz=timezone.utc).timestamp()
+        )
 
         t = int(current_timestamp / self.time_step)
         return self.hotp.generate_code(t)
