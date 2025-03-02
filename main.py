ed14d053770a1ba545b578b57a92e1dfc9cc3ad0
Jordan Tepper <12506217+HeroProtagonist@users.noreply.github.com>
2025-03-02 13:13:44 -0500

    Update cli commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import click
-from db.models import Group, Account
+from db.models import Group, Account, Tag
 from src.totp import Totp
+from src.secret import Secret
 from datetime import timezone, datetime
 
 import time
@@ -27,16 +28,28 @@ def cli():
 @click.argument("service")
 @click.argument("seed")
 @click.argument("name", default="")
-def add(service, seed, name):
+@click.option("-t", "--tag", default=[], help="tags applied to apply", multiple=True)
+def add(service, seed, name, tag):
     group, _ = Group.objects.get_or_create(name="work")
-    print(group)
-    click.echo(f"Adding #{service}, #{seed}, #{name}")
-    Account.objects.create(service=service, seed=seed, name=name, group=group)
+    click.echo(f"Adding {service}, {seed}, {name}")
+
+    account = Account.objects.create(service=service, seed=seed, name=name, group=group)
+
+    if tag:
+        for t in tag:
+            tag_object = Tag.objects.create(group=group, name=t)
+            tag_object.account.add(account)
 
 
 @cli.command()
-def remove():
-    click.echo("Removing")
+@click.argument("id")
+def remove(id):
+    group, _ = Group.objects.get_or_create(name="work")
+    account = group.account_set.get(id=id)
+
+    click.echo(f"Removing {account.service}: {account.name}")
+
+    account.delete()
 
 
 @cli.command()
@@ -44,7 +57,8 @@ def remove():
 def code(id):
     group, _ = Group.objects.get_or_create(name="work")
     account = group.account_set.get(id=id)
-    totp = Totp(account.seed, code_digits=6)
+
+    totp = Totp(Secret.from_base32(account.seed), code_digits=6, algorithm="sha1")
 
     while 1:
         start = datetime.now(tz=timezone.utc).second % 30
@@ -58,11 +72,12 @@ def list():
     table.add_column("ID")
     table.add_column("Service", style="cyan")
     table.add_column("Name", style="magenta")
+    table.add_column("Tags")
 
     accounts = Account.objects.all()
 
     for account in accounts:
-        table.add_row(str(account.id), account.service, account.name)
+        table.add_row(str(account.id), account.service, account.name, account.tags())
 
     console = Console()
     console.print(table)
